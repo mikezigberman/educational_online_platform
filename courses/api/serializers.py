9fe33300df81c5f6ef613fefb84dcf42abfc142a
Mike Zigberman <mikezigberman@yahoo.com>
2023-07-28 19:30:39 +0200

    eop: adding permissions to views

--- courses/api/serializers.py
+++ courses/api/serializers.py
@@ -13,7 +13,6 @@ class ModuleSerializer(serializers.ModelSerializer):
 
 class CourseSerializer(serializers.ModelSerializer):
     modules = ModuleSerializer(many=True, read_only=True)
-    
     class Meta:
         model = Course
         fields = ['id', 'subject', 'title', 'slug', 'overview', 'created', 'owner', 'modules']
