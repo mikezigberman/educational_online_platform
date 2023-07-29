8ea24a61c07f90edac32935ccb488bdbd3b7e86f
Mike Zigberman <mikezigberman@yahoo.com>
2023-07-29 21:46:33 +0200

    eop: adding chat app and bugfix

--- educa/educa/urls.py
+++ educa/educa/urls.py
@@ -5,7 +5,6 @@ from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
 from courses.views import CourseListView
 
-
 urlpatterns = [
     path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
     path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
@@ -15,6 +14,7 @@ urlpatterns = [
     path('students/', include('students.urls')),
     path('__debug__/', include('debug_toolbar.urls')),
     path('api/', include('courses.api.urls', namespace='api')),
+    path('chat/', include('chat.urls', namespace='chat')),
 ]
 
 if settings.DEBUG:
